12c9a2444a9a2d7233095f9a5b441937539aba5c
ggb367 <gilbert.martinez675@utexas.edu>
2018-11-09 13:16:18 -0600

    following tutorials from gazebo and ros for lidar and navigation, need to clean up files

---
+++ BoxBot_ws/src/build/robot_setup_tf/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/gilbert/Documents/MAPPOUE/BoxBot_ws/src/robot_setup_tf
+# Build directory: /home/gilbert/Documents/MAPPOUE/BoxBot_ws/src/build/robot_setup_tf
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
